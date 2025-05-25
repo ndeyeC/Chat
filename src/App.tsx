694249119379ec02810b70680ff7f02cdd397a72
03alamine <mentalisum20@gmail.com>
2025-05-25 16:38:53 +0000

    Je te passe main

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './components/auth/AuthContext';
-import PrivateRoute from './components/auth/PrivateRoute';
-import AdminPage from './pages/AdminPage';
-import UserPage from './pages/UserPage';
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
-import GroupChatPage from './pages/GroupChatPage'; 
-import Register from './pages/Register';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./components/auth/AuthContext";
+import PrivateRoute from "./components/auth/PrivateRoute";
+import AdminPage from "./pages/AdminPage";
+import UserPage from "./pages/UserPage";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import GroupChatPage from "./pages/GroupChatPage";
+import Register from "./pages/Register";
+//import Intervention from './components/Intervention';
 
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 
-import ResetPasswordPage from './pages/ResetPasswordPage';
-
-
-
-import CreateGroupPage from './pages/groupe/CreateGroup';  // Importer la page de création de groupe
-import UserAdminControl from './pages/user/UserAdminControl';
+import CreateGroupPage from "./pages/groupe/CreateGroup"; // Importer la page de création de groupe
+import UserAdminControl from "./pages/user/UserAdminControl";
 
 function App() {
   return (
-    
     <Router>
       <AuthProvider>
         <Routes>
@@ -27,41 +24,46 @@ function App() {
 
           <Route path="/register" element={<Register />} />
 
-<Route path="/reset" element={<ResetPasswordPage />} />
-<Route path="/admin/users" element={<UserAdminControl />} />
-
+          <Route path="/reset" element={<ResetPasswordPage />} />
+          {/*  <Route path="/intervention" element={<Intervention />} /> */}
+          <Route path="/admin/users" element={<UserAdminControl />} />
 
           {/* Page de connexion */}
           <Route path="/login" element={<LoginPage />} />
 
           {/* Route admin protégée */}
-          <Route path="/admin" element={
-            <PrivateRoute adminOnly>
-              <AdminPage />
-            </PrivateRoute>
-          } />
+          <Route
+            path="/admin"
+            element={
+              <PrivateRoute adminOnly>
+                <AdminPage />
+              </PrivateRoute>
+            }
+          />
 
           {/* Page de création de groupe */}
           <Route path="/create-group" element={<CreateGroupPage />} />
 
           {/* Page utilisateur protégée */}
-          <Route path="/user" element={
-            <PrivateRoute>
-              <UserPage />
-            </PrivateRoute>
-          } />
+          <Route
+            path="/user"
+            element={
+              <PrivateRoute>
+                <UserPage />
+              </PrivateRoute>
+            }
+          />
 
-          <Route path="/group/:groupId" element={
-      <PrivateRoute>
-      <GroupChatPage />
-      </PrivateRoute>
-     } />
-     
+          <Route
+            path="/group/:groupId"
+            element={
+              <PrivateRoute>
+                <GroupChatPage />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </AuthProvider>
-
-
-
     </Router>
   );
 }
